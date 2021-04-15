c9ff3d6f633a94a1e141ac408989c7adc00680b5
0-k <martin.t.klein@gmail.com>
2021-04-15 17:25:58 +0200

    refactoring parser of polling results

--- data/parser.py
+++ data/parser.py
@@ -1,7 +1,11 @@
-from bs4 import BeautifulSoup
 import requests
+import os.path
+import datetime as dt
+from bs4 import BeautifulSoup
+
 from config.config import config
-from src.parties import Party
+import src.parties as parties
+
 
 
 class Parser:
@@ -9,27 +13,53 @@ class Parser:
     def __init__(self):
         self.soup = None
         self.__query_latest()
+        self.__make_soup()
 
-    @staticmethod
-    def __query_latest():
+    def __query_latest(self):
+        if self.__has_already_been_cached_today():
+            return
+        else:
+            self.__make_request()
+
+    def __has_already_been_cached_today(self):
+        if self.__cache_did_change_today():
+            return True
+        return False
+
+    def __cache_did_change_today(self):
+        if self.__cache_does_not_exist():
+            return False
+        today = dt.datetime.now().date()
+        filetime = dt.datetime.fromtimestamp(os.path.getctime(config['caching_filename']))
+        return filetime.date() == today
+
+    def __cache_does_not_exist(self):
+        return not os.path.exists(config['caching_filename'])
+
+    def __make_request(self):
+        print('making request')
         r = requests.get(config['poll_url'])
-        file = open('polls_cashed.html', "w")
+        file = open(config['caching_filename'], "w")
         file.write(r.text)
         file.close()
 
     def __make_soup(self):
-        with open('polls_cashed.html') as fp:
+        print('making soup')
+        with open(config['caching_filename']) as fp:
             self.soup = BeautifulSoup(fp, 'html.parser')
 
-    def get_latest_polls_of(self, party):
-        if self.soup is None:
-            self.__make_soup()
-        return self.soup.find(id=party.id).find_all("td")
+    def get_latest_percentages_of(self, party):
+        soup = self.soup.find(id=party.id).find_all("td")[1:9]
+        percentages = [float(item.text.split(' ')[0].replace(',', '.')) for item in soup]
+        return percentages
+
+    def get_latest_dates_of_polls(self):
+        soup = self.soup.find(id='datum').find_all("td")[1:9]
+        dates = [dt.datetime.strptime(item.text, '%d.%m.%Y').date() for item in soup]
+        return dates
 
 
 if __name__ == '__main__':
     parser = Parser()
-    union = Party(name='UNION', id='cdu')
-    percentages = parser.get_latest_polls_of(union)
-    for percentage in percentages[1:9]:
-        print(float(percentage.text.split(' ')[0].replace(',', '.')))
+    percentages = parser.get_latest_percentages_of(parties.GRUENE)
+    dates = parser.get_latest_dates_of_polls()
